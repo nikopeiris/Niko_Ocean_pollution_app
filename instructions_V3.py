7345aa8c5ef515291f1fe6b63e5466abcf992809
nikopeiris <nikopeiris123@gmail.com>
2022-07-07 09:51:35 +1200

    Create instructions_V3.py

---
+++ instructions_V3.py
@@ -0,0 +1,19 @@
+def instruction():
+    print("""                               **INSTRUCTIONS**
+    Welcome to the Ocean Pollution app. This app consists of few things.
+    1. Waste calculator:
+            Where you calculate the amount of waste you create compared to the average person,
+            and how to reduce it.
+    2. Solutions:
+            List of solutions you can perform to reduce waste.
+    3. Quiz:
+            Quiz about Ocean pollution and Ocean issues.
+    4. Instructions:
+            Just in case you wanted to see the instructions again
+
+    **If you want to go back to Home section, Type Home[H]**
+    **If you are presented with options to choose from, Enter the corresponding number to the option**
+    **If you want to quit the program type, quit[q]**""")
+
+
+instruction()
