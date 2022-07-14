6cd070820546cfc711bfe56b4483e40ee8dad696
nikopeiris <nikopeiris123@gmail.com>
2022-07-14 22:50:31 +1200

    Create quiz_question__answer_V1.py

---
+++ quiz_question__answer_V1.py
@@ -0,0 +1,33 @@
+questions_and_answers = [["""{}. Where does most ocean pollution come from?
+ |1. marine activities.\n |2. land activities.\n |3. both, it is fairly evenly divided.""", 2, """More than 80% of all,
+ocean pollution comes from land. oil from cities and factories washes down drains and into the ocean."""],
+                         ["""{}. Elevated water temperature can cause corals to do what?
+ |1. grow too quickly.\n |2. bleach.\n |3. eat themselves.""", 2,
+                         """Elevated temperatures trigger mass episodes bleaching"""],
+                         ["""{}. Industrial fishing is thought to have wiped out what percentage of large predatory fish?
+ |1. 25%.\n |2. 55%.\n |3. 66%.""", 3,
+                          """In the 20th century, humans reduced the biomass of predatory fish by more than two-thirds"""],
+                         ["""{}. What area of the ocean is suffering most from habitat destruction?
+ |1. seafloor.\n |2. deep sea\n |3. coasts.""", 3,
+                          """Coastal areas, with their closeness to human population centers have suffered disproportionately"""],
+                         ["""{}. In the summer of 2014, researchers recorded the highest global mean sea-surface temperatures
+in history. where was the warming anomaly concentrated?
+ |1. south pacific\n |2. north pacific\n |3. indian.""", 2,
+                          """The 2014 global ocean warming was mostly due to the north pacific"""],
+                         ["""{}. What is the rate of sea-level rise over the past century?
+ |1. 0.01 to 0.04 inch per year,(0.254 mm to 1.016 mm per year)\n |2. 0.04 to 0.1 inch per year,(1.016 mm to 2.54 mm per year)\n |3. 0.1 to 0.12 inch per year,(2.54 mm to 3.048 mm per year)""",
+                          2, """Records and research show tha sea level has been steadily rising at a rate of about, 
+0.04 to 0.1 inches per year(1.016 mm to 2.54 mm per year) since 1900."""],
+                         ["""{}. Where is the world's largest marine protected area?
+ |1. coral sea\n |2. pitcairn islands\n |3. hawaiian islands""", 1,
+                          """The natural park of the coral sea protects 1.3 million square kilometers
+(501,930 square miles) of marine ecosystems"""],
+                         ["""{}. Which fishery is not depleted?
+ |1. atlantic cod\n |2. south pacific bluefin tuna\n |3. atlantic herring""", 3,
+                          """Atlantic herring fish stocks range from underexploited to recovering. 
+Atlantic cod and pacific bluefin tuna fisheries are both depleted"""],
+                         ["""{}. What animals are most at risk from ocean acidification?
+ |1. shellfish\n |2. marine mammals\n |3. bottom-feeding fish""", 1,
+                          """Ocean acidity has already increased more than 30% from pre-industrial levels.
+shellfish are most risk from Ocean acidification"""]]
+
