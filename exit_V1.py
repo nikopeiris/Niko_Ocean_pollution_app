a6727058c28a148fb3499c9bc8e8ca7f71ac74c1
nikopeiris <nikopeiris123@gmail.com>
2022-07-14 22:50:42 +1200

    Create exit_V1.py

---
+++ exit_V1.py
@@ -0,0 +1,12 @@
+# this is displayed if the user decided to exit/ quit the app
+import time
+
+
+def exit_app():
+    print(f"Thank You, {name} for using the Ocean Care app")
+    print("________________________________________")
+    time.sleep(1)
+    exit()
+
+
+exit_app()
