1d2fdc22a2b5d4b49ef09961ec34007ea832028a
nikopeiris <nikopeiris123@gmail.com>
2022-07-14 22:50:53 +1200

    Create displayed_info_V1.py

---
+++ displayed_info_V1.py
@@ -0,0 +1,10 @@
+displayed_info = ["""|You produce the exact amount of garbage produced by an average New Zealander,
+|Which is 62000g a Week and 3200000g a Year!\n|Overall You're doing Great!""",
+                  """|An average New Zealander produces around 62000g garbage a Week, and
+|3200000g garbage a Year.
+|You produce {0}g {1} garbage in a {2} compared to an average New Zealander!
+|Which is {3}% {1} garbage!\n|Overall You produce {1} garbage!""",
+                  """|An average New Zealander produces around 62kg garbage a Week, and
+|3200kg garbage a Year.
+|You produce {0}kg {1} garbage in a {2} compared to an average New Zealander!
+|Which is {3}% {1} garbage!\n|Overall You produce {1} garbage!"""]
